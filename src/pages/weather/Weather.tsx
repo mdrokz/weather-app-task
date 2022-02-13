a3e4d9827a7bd2df8e06021a12a79bdab32befa4
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-02-13 18:45:50 +0530

    Implement weather api in component

--- src/pages/weather/Weather.tsx
+++ src/pages/weather/Weather.tsx
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DOMPurify from 'dompurify';
+import { WeatherData } from "../../utils/types";
+import Http from "../../utils/http";
 
 function debounce<T>(this: T, func: Function, timeout = 500) {
     let timer: any;
-    console.log('vruhh?',this,func);
+    console.log('vruhh?', this, func);
     return (...args: T[]) => {
         clearTimeout(timer);
         timer = setTimeout(() => { func.apply(this, args); }, timeout);
     };
 }
 
-const Weather = () => {
+const Weather = ({ lat, lon }: { lat: string, lon: string }) => {
+
+    const [weather, setWeather] = useState<WeatherData>(Object.create({}));
+
+    const init = async () => {
+
+        const data = await Http.get_weather(lat, lon);
+
+        setWeather(data);
+    }
+
+    useEffect(() => {
+
+        init();
+
+    }, [])
+
+    return (<>
+        <div>
+
+        </div>
+    </>)
+}
+
+const WeatherPage = () => {
 
     const [markdown, setMarkDown] = useState('');
 
@@ -20,7 +46,7 @@ const Weather = () => {
 
         console.log(e);
     }
-    const processChange = debounce((e:any) => onChange(e));
+    const processChange = debounce((e: any) => onChange(e));
 
     return (<>
         <div className="container">
@@ -35,4 +61,4 @@ const Weather = () => {
 }
 
 
-export default Weather;
+export default WeatherPage;
