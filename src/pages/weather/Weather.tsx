f554f8f5473c76d9e78bed4f70e36c6323988f44
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-02-13 22:28:58 +0530

    add support for custom weather tag

--- src/pages/weather/Weather.tsx
+++ src/pages/weather/Weather.tsx
@@ -1,7 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import DOMPurify from 'dompurify';
+
+import ReactDOM from 'react-dom';
+
 import { WeatherData } from "../../utils/types";
 import Http from "../../utils/http";
+
+import './Weather.css';
+
 
 function debounce<T>(this: T, func: Function, timeout = 500) {
     let timer: any;
@@ -30,31 +36,58 @@ const Weather = ({ lat, lon }: { lat: string, lon: string }) => {
     }, [])
 
     return (<>
-        <div>
-
-        </div>
+        {weather?.main &&
+            <div>
+                <h2>Country: {weather.name}</h2>
+                <div className="container">
+                    <span>Clouds: {weather.clouds.all}</span>
+                    <span>Timezone: {weather.timezone}</span>
+                    <span>Weather: {weather.weather[0].main}</span>
+                    <span>Temperature: {weather.main.temp}</span>
+                </div>
+            </div>
+        }
     </>)
 }
 
 const WeatherPage = () => {
 
     const [markdown, setMarkDown] = useState('');
+    const [loc, setLoc] = useState<{ lat: string, lon: string }>({ lat: '', lon: '' });
 
+    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const value = e.target.value;
 
-    const onChange = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        // const sanitized = DOMPurify.sanitize(e.target);
+        const weather = value.substring(value.indexOf('weather')).match(/{(.*?)}/g);
 
-        console.log(e);
+        var lat = '';
+        var lon = '';
+
+        if (weather && weather.length > 0) {
+            lat = weather[0].replace('{', '').replace('}', '');
+            lon = weather[1].replace('{', '').replace('}', '');
+            setLoc({ lat, lon });
+        }
+
+        const sanitized = DOMPurify.sanitize(value);
+
+        // console.log(sanitized);
+        // console.log(value, lat, long);
+        console.log(lat, lon);
+
+        setMarkDown(sanitized);
     }
     const processChange = debounce((e: any) => onChange(e));
 
     return (<>
         <div className="container">
-            <textarea placeholder="Enter markdown" onKeyUp={processChange as any}>
+            <textarea placeholder="Enter markdown" onChange={processChange as any}>
 
             </textarea>
-            <div dangerouslySetInnerHTML={{ __html: markdown }}>
-
+            <div className="weather_data">
+                <div dangerouslySetInnerHTML={{ __html: markdown }}>
+                </div>
+                {loc.lat != '' && loc.lon != '' && <Weather lat={loc.lat} lon={loc.lon}></Weather>}
             </div>
         </div>
     </>)
