ce25ac9cd82bc54093b472ab1096ae5d8d151b1b
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-02-14 01:49:54 +0530

    added upload functionality

--- src/pages/image/Image.tsx
+++ src/pages/image/Image.tsx
@@ -1,24 +1,38 @@
 import React, { useState, ChangeEvent } from "react"
+import Http from "../../utils/http";
 import './Image.css';
 const Image = () => {
 
     const [imageUrl, setImageUrl] = useState('');
 
+    const [shortUrl, setShortUrl] = useState('');
+
     const uploadImage = (e: ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
 
-        if (files) setImageUrl(URL.createObjectURL(files[0]))
+        if (files) {
+            setImageUrl(URL.createObjectURL(files[0]))
+            Http.upload_image(files[0]).then(url => {
+                setShortUrl(url);
+            })
+        }
 
-        console.log(e.target.files);
     }
+
 
     return (<>
         <div className="container">
             <div>
-                <img src={imageUrl} alt='Selected Image'></img>
-                <input type="file" onChange={uploadImage} />
+                <div>
+                    <img src={imageUrl} alt='Selected Image'></img>
+                </div>
+                <div>
+                    <a href={shortUrl}>Image URL</a>
+                </div>
+                <div>
+                    <input type="file" onChange={uploadImage} />
+                </div>
             </div>
-
         </div>
     </>)
 }
