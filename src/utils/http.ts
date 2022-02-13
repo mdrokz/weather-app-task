0e26788aaf2c5f16cbd1b1130f9648d9a45f52b8
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-02-14 01:50:11 +0530

    Implemented upload_image method

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -6,8 +6,18 @@ export default class Http {
 
     static WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat={1}&lon={2}.0&appid=${this.WEATHER_API_KEY}`;
 
-    constructor() {
 
+    static BASE_API_URL = 'http://localhost:8080/api/'
+
+    static async upload_image(image_string: Blob) {
+        const form_data = new FormData();
+
+        form_data.append('image', image_string);
+
+        return await (await fetch(this.BASE_API_URL + 'upload', {
+            method: 'POST',
+            body: form_data
+        })).text()
     }
 
     static async get_weather(lat: string, lon: string) {
